52edcd811445dbfdbd5fcc76356a37c328b59ec7
James McFeeters <jmcfeeters@scripps.edu>
2025-07-03 20:34:24 -0700

    Fix /count/x/by/y

--- DB/queries/counts.py
+++ DB/queries/counts.py
@@ -28,12 +28,8 @@ async def count_variants_by_column(by_col: str):
         res = await session.execute(
             select(IntraHostVariant, Allele, Translation, AminoAcid)
             .join(Allele, Allele.id == IntraHostVariant.allele_id, isouter=True)
-            .join(Translation, Allele.id == Translation.allele_id, isouter=True)
-            .join(
-                AminoAcid,
-                Translation.amino_acid_substitution_id == AminoAcid.id,
-                isouter=True
-            )
+            .join(Translation, Translation.id == IntraHostVariant.translation_id, isouter=True)
+            .join(AminoAcid, AminoAcid.id == Translation.amino_acid_id, isouter=True)
             .with_only_columns(text(by_col), func.count().label('count1'))
             .group_by(text(by_col))
             .order_by(text('count1 desc'))
@@ -46,12 +42,8 @@ async def count_mutations_by_column(by_col: str):
         res = await session.execute(
             select(Mutation, Allele, Translation, AminoAcid)
             .join(Allele, Allele.id == Mutation.allele_id, isouter=True)
-            .join(Translation, Allele.id == Translation.allele_id, isouter=True)
-            .join(
-                AminoAcid,
-                Translation.amino_acid_substitution_id == AminoAcid.id,
-                isouter=True
-            )
+            .join(Translation, Translation.id == Mutation.translation_id, isouter=True)
+            .join(AminoAcid, AminoAcid.id == Translation.amino_acid_id, isouter=True)
             .with_only_columns(text(by_col), func.count().label('count1'))
             .group_by(text(by_col))
             .order_by(text('count1 desc'))
