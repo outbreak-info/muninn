812b013e9cf7573da3b7d2488d546d90d87cd3b9
James McFeeters <jmcfeeters@scripps.edu>
2025-07-11 10:04:25 -0700

    Fix annotations date count query

--- DB/inserts/annotations.py
+++ DB/inserts/annotations.py
@@ -1,6 +1,5 @@
 from typing import Set
 
-
 from DB.engine import get_async_write_session
 from DB.models import Annotation, AnnotationAminoAcid
 from DB.queries.amino_acid_substitutions import get_aa_ids_for_annotation_effect
@@ -9,7 +8,7 @@ from utils.errors import DuplicateAnnotationError
 
 async def insert_annotation(a: Annotation, amino_acid_ids: Set[int]) -> int:
     # make sure we aren't making a duplicate
-    amino_acid_ids = set(amino_acid_ids) # no, you're paranoid
+    amino_acid_ids = set(amino_acid_ids)  # no, you're paranoid
     existing_id_sets = await get_aa_ids_for_annotation_effect(a.effect_id)
     if amino_acid_ids in existing_id_sets:
         raise DuplicateAnnotationError(message='No')
@@ -19,11 +18,10 @@ async def insert_annotation(a: Annotation, amino_acid_ids: Set[int]) -> int:
         await session.commit()
         await session.refresh(a)
 
-        aaas = [
+        amino_acids = [
             AnnotationAminoAcid(amino_acid_id=aaid, annotation_id=a.id)
             for aaid in amino_acid_ids
         ]
-        session.add_all(aaas)
+        session.add_all(amino_acids)
         await session.commit()
     return a.id
-
--- DB/queries/annotations.py
+++ DB/queries/annotations.py
@@ -8,6 +8,7 @@ from DB.models import IntraHostVariant, Mutation
 from parser.parser import parser
 from utils.constants import DateBinOpt
 
+
 # TODO: Add Effect to DB.models and EffectInfo to api.models
 async def get_all_annotation_effects() -> List[str]:
     async with get_async_session() as session:
@@ -24,25 +25,41 @@ async def get_all_annotation_effects() -> List[str]:
         out_data.append(r[0])
     return out_data
 
+
 async def get_annotations_by_mutations_and_collection_date(
-        effect_detail: str,
-        date_bin: DateBinOpt,
-        days: int,
-        max_span_days: int,
-        raw_query: str,
-    ) -> List[Dict]:
+    effect_detail: str,
+    date_bin: DateBinOpt,
+    days: int,
+    max_span_days: int,
+    raw_query: str,
+) -> List[Dict]:
     return await _get_annotations_by_collection_date(effect_detail, date_bin, days, max_span_days, raw_query, Mutation)
 
+
 async def get_annotations_by_variants_and_collection_date(
-        effect_detail: str,
-        date_bin: DateBinOpt,
-        days: int,
-        max_span_days: int,
-        raw_query: str,
-    ) -> List[Dict]:
-    return await _get_annotations_by_collection_date(effect_detail, date_bin, days, max_span_days, raw_query, IntraHostVariant)
+    effect_detail: str,
+    date_bin: DateBinOpt,
+    days: int,
+    max_span_days: int,
+    raw_query: str,
+) -> List[Dict]:
+    return await _get_annotations_by_collection_date(
+        effect_detail,
+        date_bin,
+        days,
+        max_span_days,
+        raw_query,
+        IntraHostVariant
+    )
 
 
+# todo:
+#  this is now a bit broken because of the many-many relationship between amino acids and annotations
+#  for example: say that amino acid changes aaX and aaY cause effect E.
+#  say a sample S has a variant with aaX, but not aaY
+#  under the query as it stands, we will count 1 annotation for E on the date for S
+#  but have we really seen that annotation if we saw only one of the two amino acid changes flagged
+#  as necessary to achieve the effect?
 async def _get_annotations_by_collection_date(
     effect_detail: str,
     date_bin: DateBinOpt,
@@ -51,7 +68,6 @@ async def _get_annotations_by_collection_date(
     raw_query: str,
     table: Type[IntraHostVariant] | Type[Mutation]
 ) -> List[Dict]:
-
     user_where_clause = ''
     if raw_query is not None:
         user_where_clause = f'and ({parser.parse(raw_query)})'
@@ -105,10 +121,14 @@ async def _get_annotations_by_collection_date(
                         inner join lineage_systems ls on ls.id = l.lineage_system_id
                         left join translations t on t.id = VM.translation_id
                         left join amino_acids aa on aa.id = t.amino_acid_id
-                        inner join annotations a on a.amino_acid_id = aa.id
+                        inner join annotations_amino_acids aaa on aaa.amino_acid_id = aa.id
+                        inner join annotations a on a.id = aaa.annotation_id
                         inner join effects e on e.id = a.effect_id
                         inner join annotations_papers ap on ap.annotation_id = a.id
-                        where num_nulls(collection_end_date, collection_start_date) = 0 and e.detail = '{effect_detail}' {user_where_clause}
+                        inner join papers p on p.id = ap.paper_id
+                        where num_nulls(collection_end_date, collection_start_date) = 0 
+                        and e.detail = '{effect_detail}' 
+                        {user_where_clause}
                     )
                     where collection_span <= {max_span_days}
                 )
@@ -119,8 +139,10 @@ async def _get_annotations_by_collection_date(
     out_data = []
     for r in res:
         date = date_bin.format_iso_chunk(r[0], r[1])
-        out_data.append({
-            "date": date,
-            "n": r[2]
-        })
+        out_data.append(
+            {
+                "date": date,
+                "n": r[2]
+            }
+        )
     return out_data
