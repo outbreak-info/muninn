75322a735258fb4df6e07499de415f7a55eba420
gp201 <praneethg2001@gmail.com>
2026-01-21 16:20:00 -0800

    feat: Add mutdiffsel to StandardPhenoMetricNames

--- utils/constants.py
+++ utils/constants.py
@@ -221,6 +221,7 @@ class StandardPhenoMetricNames:
     ferret_sera_escape = 'ferret_sera_escape'
     mouse_sera_escape = 'mouse_sera_escape'
     entry_in_sa26_and_sa23_293t_cells = 'entry_in_sa26_and_sa23_293t_cells'
+    mutdiffsel = 'mutdiffsel'
 
 
 class ConstraintNames:
