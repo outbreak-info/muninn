d2a2582faec2158a793d98fbe505cfe21decb161
Praneeth <pgangavarapu@scripps.edu>
2026-09-25 09:42:35 -0700

    perf: intersect sample bitmaps before unnesting (#23)
    * perf: intersect sample bitmaps before unnesting
    
    * fix: count per row rather than per change, since samples are restricted
    to one bin per allele/AA

--- DB/queries/prevalence.py
+++ DB/queries/prevalence.py
@@ -144,16 +144,22 @@ async def get_pheno_values_and_mutation_counts(
             from {TableNames.samples} s
             left join {TableNames.geo_locations} gl on gl.id = s.{ColumnNames.geo_location_id}
             inner join {TableNames.samples_lineages} sl on sl.{ColumnNames.sample_id} = s.id
-            inner join {TableNames.lineages} l on l.id = sl.{ColumnNames.lineage_id} 
+            inner join {TableNames.lineages} l on l.id = sl.{ColumnNames.lineage_id}
             {user_where_clause}
+        ),
+        matching_samples_bm as (
+            select coalesce(rb_build_agg(sample_id), rb_build('{{}}')) as bm
+            from matching_samples
         )
         select aas.ref_aa,
                aas.position_aa,
                aas.alt_aa,
                pmv.value,
-               count(distinct matching_samples.sample_id) as count
-        from matching_samples
-        inner join {TableNames.cns_samples_by_amino_acid} csaa on csaa.{ColumnNames.samples_present} @> matching_samples.sample_id
+               rb_and_cardinality(
+                   rb_or_agg(csaa.{ColumnNames.samples_present}),
+                   (select bm from matching_samples_bm)
+               ) as count
+        from {TableNames.cns_samples_by_amino_acid} csaa
         inner join {TableNames.amino_acids} aas on aas.id = csaa.{ColumnNames.amino_acid_id}
         inner join {TableNames.phenotype_metric_values} pmv on pmv.{ColumnNames.amino_acid_id} = aas.id
         inner join {TableNames.phenotype_metrics} pm on pm.id = pmv.{ColumnNames.phenotype_metric_id}
